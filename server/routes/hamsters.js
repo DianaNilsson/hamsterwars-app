04a0af6bd9bbee2283342843c3001ef13cc6c0f3
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-16 15:45:36 +0200

    update post games route

--- server/routes/hamsters.js
+++ server/routes/hamsters.js
@@ -1,23 +1,129 @@
-const { db } = require('./../firebase');
+const { db, fieldValue } = require('./../firebase');
 
 const { Router } = require('express');
 
 const router = new Router();
 
-//Get all hamsters
+//Get all games
 router.get('/', async (req, res) => {
 
     try {
-        let hamsters = [];
+        let games = [];
 
-        let getHamsters = await db.collection('hamsters').get();
+        let getGames = await db.collection('games').get()
 
+        getGames.forEach(game => {
+            games.push(game.data())
+        });
+
+        res.status(200).send(games)
+
+    } catch {
+        console.log(err)
+        res.status(500).send(err);
+    }
+})
+
+
+//Post games
+router.post('/', async (req, res) => {
+
+    try {
+
+        let contestantsId = []
+        let contestants = []
+        let winner;
+        let winnerDocId;
+        let loosers = [];
+        let loosersDocId = [];
+        let gameId;
+
+        //Get the id:s of all competing hamsters
+        req.body.contestants.forEach(hamster => {
+            contestantsId.push(hamster.id);
+        });
+
+        //Get all the hamster docs where id matches the contestants id:s
+        let getHamsters = await db.collection('hamsters').where('id', 'in', contestantsId).get();
+
+        //Push to the contestants array
         getHamsters.forEach(doc => {
-            hamsters.push(doc.data());
+            contestants.push(doc.data());
+
+            //Set winner
+            if (doc.data().id == req.body.winner.id) {
+                winner = doc.data();
+                winnerDocId = doc.id;
+            }
+            //Set loosers
+            else {
+                loosers.push(doc.data());
+                loosersDocId.push(doc.id);
+            }
         })
 
+        //Game id will increase with one for each game
+        try {
+            let getCounter = await db.collection('games').doc('counter').get()
+
+            if (getCounter.data() !== undefined) {
+                gameId = getCounter.data().gameCount + 1;
+            } else {
+                gameId = 1;
+            }
+        } catch (err) {
+            console.log('Error increasing game id', err);
+        }
+
+
+        //Set/update firestore with a batched write
+
+        //Increment the field's current value by 1
+        const increment = fieldValue.increment(1);
+
+        let batch = db.batch();
+
+        //Save the game
+        batch.set(db.collection('games').doc(), {
+            id: gameId,
+            timeStamp: fieldValue.serverTimestamp(),
+            contestants,
+            winner
+        })
+
+        //Set the counter (count all games)
+        batch.set(db.collection('games').doc('counter'), {
+            gameCount: increment
+        }, {
+            merge: true
+        })
+
+        //Update winner hamster
+        batch.update(db.collection('hamsters').doc(winnerDocId), {
+            wins: increment,
+            games: increment
+        })
+
+        //Update loosers hamsters
+        loosersDocId.forEach(docId => {
+            batch.update(db.collection('hamsters').doc(docId), {
+                defeats: increment,
+                games: increment
+            })
+        });
+
+        //Commit writes (as a single unit)
+        try {
+            batch.commit()
+            console.log('Firestore updated successfully!')
+        } catch (err) {
+            console.log('Error updating Firestore.', err)
+        }
+
+        //Send response
         res.status(200).send({
-            hamsters
+            gameId
+            // msg: `Game finished and ${winner.name} won, congratulations!`
         })
 
     } catch (err) {
@@ -26,80 +132,5 @@ router.get('/', async (req, res) => {
     }
 })
 
-//Get random hamsters
-router.get('/random/:number?', async (req, res) => {
 
-    let statusCode = 500
-
-    try {
-        let hamsters = [];
-        let contestants = [];
-
-        //Later I will create a separate counter instead of returning all hamsters
-        //When I know how many docs that exists in the collection I can randomly choose numbers and match with id:s in a query
-        let getHamsters = await db.collection('hamsters').get()
-
-        getHamsters.forEach(doc => {
-            hamsters.push(doc.data());
-        })
-
-        //Choose random and unique contestants (one or more)
-        //Check if param is set
-        if (req.params.number) {
-
-            //Check that req.params.number is a valid number
-            if (req.params.number > 0 && req.params.number <= hamsters.length) {
-                for (let i = 0; i < req.params.number * 1; i++) {
-                    let rand = Math.floor(Math.random() * hamsters.length);
-                    let randomHamster = hamsters.splice(rand, 1);
-                    contestants.push(randomHamster[0]);
-                }
-            } else {
-                statusCode = 404;
-                throw 'This is no valid number, please choose the number of random hamsters that you want to see';
-            }
-        }
-        //If no param is set, choose one 
-        else {
-            for (let i = 0; i < 1; i++) {
-                let rand = Math.floor(Math.random() * hamsters.length);
-                contestants.push(hamsters[rand]);
-            }
-        }
-
-        res.status(200).send(contestants)
-
-    } catch (err) {
-        console.log(err)
-        res.status(statusCode).send(err);
-    }
-})
-
-//Get hamster by id
-router.get('/:id', async (req, res) => {
-
-    let statusCode = 500;
-
-    try {
-        let hamster;
-
-        let getHamsters = await db.collection('hamsters').where("id", "==", req.params.id * 1).get()
-
-        getHamsters.forEach(doc => {
-            hamster = (doc.data())
-        })
-
-        //Check if the hamster is set and send response
-        if (hamster !== undefined) {
-            res.status(200).send(hamster)
-        } else {
-            statusCode = 404;
-            throw 'This id does not match any of the hamsters id:s, please try again!';
-        }
-    } catch (err) {
-        console.log(err)
-        res.status(statusCode).send(err);
-    }
-})
-
-module.exports = router
+module.exports = router;
