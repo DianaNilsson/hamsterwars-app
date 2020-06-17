aea0c7eb707417232b637a4c74af73a99516cc5e
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-17 08:06:08 +0200

    serve static images

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 const serverPort = process.env.PORT || 3001;
 
 //Serve static images 
-app.use('/assets', express.static(__dirname + 'hamsters'))
+app.use('/assets', express.static(__dirname + '/assets'))
 
 // Serve the frontend
 app.use(express.static(__dirname + '/../build'));
