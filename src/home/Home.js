e6e325058ac0ba6e8cca6d9e1c006b8b0e195a7d
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-17 12:10:50 +0200

    design and content home view

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home</h2>
-        </div>
+        <section className="home-section">
+            <h3 className="subheader">Welcome to this Hamsterwar Application</h3>
+            <p>In this playful app you can vote for the cutest hamster. You can also upload your own hamster or just explore cute hamster pictures and funny hamster facts.</p>
+            <p>Detta projekt är en del i kursen Effektiv Frontend-utveckling som ingår i utbildningen Frontend-utveckling på IT-Högskolan i Göteborg/Stockholm. I projektet har frontend och backend kombinerats med React på frontend-sidan och Node.js, Express och cloud-database Google Firestore på backendsidan. Detta är mitt första React-projekt och det har varit mycket kul! <span className="sign-by">/Diana</span></p>
+        </section>
     );
 };
 
