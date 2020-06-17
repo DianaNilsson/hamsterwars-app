c365b72bc62c75f729fbbc3cba3c70110597f912
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-17 09:40:01 +0200

    add replace to enviroment variable

--- server/firebase.js
+++ server/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccount.json");
+// const serviceAccount = require("./serviceAccount.json");
 
 if (process.env.NODE_ENV === 'production') {
 
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
             "type": process.env.FIREBASE_TYPE,
             "project_id": process.env.FIREBASE_PROJECT_ID,
             "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-            "private_key": process.env.FIREBASE_PRIVATE_KEY,
+            "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
             "client_email": process.env.FIREBASE_CLIENT_EMAIL,
             "client_id": process.env.FIREBASE_CLIENT_ID,
             "auth_uri": process.env.FIREBASE_AUTH_URI,
