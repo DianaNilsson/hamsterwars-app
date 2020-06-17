41abf77747a9181b541149f27524980b83a182c6
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-17 09:32:52 +0200

    add enviroment variables

--- server/firebase.js
+++ server/firebase.js
@@ -4,7 +4,18 @@ const serviceAccount = require("./serviceAccount.json");
 if (process.env.NODE_ENV === 'production') {
 
     admin.initializeApp({
-        credentials: JSON.parse(process.env.GCS_KEYFILE),
+        credential: admin.credential.cert({
+            "type": process.env.FIREBASE_TYPE,
+            "project_id": process.env.FIREBASE_PROJECT_ID,
+            "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+            "private_key": process.env.FIREBASE_PRIVATE_KEY,
+            "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+            "client_id": process.env.FIREBASE_CLIENT_ID,
+            "auth_uri": process.env.FIREBASE_AUTH_URI,
+            "token_uri": process.env.FIREBASE_TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+        }),
         databaseURL: "https://hamsterwars-project.firebaseio.com"
     });
 }
