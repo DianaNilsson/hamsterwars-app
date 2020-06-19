7163ec83c149e4073491cf9366360dcf4cde0212
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-06-19 07:59:16 +0200

    added bottom hamsters

--- src/stats/Stats.js
+++ src/stats/Stats.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './Stats.css'
 import Podium from './Podium';
+import { RiEmotionSadLine } from 'react-icons/ri';
 
 const Stats = () => {
 
     const [topHamsters, setTopHamsters] = useState([]);
+    const [bottomHamsters, setBottomHamsters] = useState([]);
 
     useEffect(() => {
         let ignore = false;
         const fetchTopHamsters = async () => {
             try {
-                const response = await fetch("/charts/top");
-                const result = await response.json();
+                const responseTop = await fetch("/charts/top");
+                const responseBottom = await fetch("/charts/bottom");
+
+
+                const resultTop = await responseTop.json();
+                const resultBottom = await responseBottom.json();
 
                 if (!ignore) {
-                    setTopHamsters(result);
+                    setTopHamsters(resultTop);
+                    setBottomHamsters(resultBottom);
                     return (() => { ignore = true; });
                 }
             } catch (error) {
@@ -28,6 +35,21 @@ const Stats = () => {
         <section className="stats-section">
 
             <Podium topHamsters={topHamsters} />
+
+            <h3 className="heading bottom-hamsters-header">The hamsters that really need your love though are:</h3>
+
+            <div className="bottom-container">
+                {bottomHamsters.map(bottomHamster => (
+                    <div key={setBottomHamsters.id} className="bottom-hamster-item">
+                        <img src={`/assets/${bottomHamster.imgName}`} alt={bottomHamster.imgName} />
+                        <p>{bottomHamster.name}</p>
+                    </div>
+                ))}
+            </div>
+
+            <p className="subheader">they are the most losing hamsters...</p>
+            <RiEmotionSadLine className="sad-icon" />
+
 
         </section >
     );
